753be351977c2aa988cb81e412aabea388122089
Slawomir Borowiec <przodownik@tlen.pl>
2015-05-01 23:14:49 +0200

    jobRegistryBeanPostProcessor added

--- src/main/java/pl/java/scalatech/config/StandaloneConfiguration.java
+++ src/main/java/pl/java/scalatech/config/StandaloneConfiguration.java
@@ -2,7 +2,9 @@ package pl.java.scalatech.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
@@ -15,15 +17,13 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class StandaloneConfiguration {
     private static final int MAX_THREADS = 8;
     private static final int MIN_THREADS = 4;
+
     @Bean
     public DataSource dataSource() {
         EmbeddedDatabaseBuilder embeddedDatabaseBuilder = new EmbeddedDatabaseBuilder();
         return embeddedDatabaseBuilder.addScript("classpath:org/springframework/batch/core/schema-drop-h2.sql")
-                .addScript("classpath:org/springframework/batch/core/schema-h2.sql").setType(EmbeddedDatabaseType.H2)
-                .build();
+                .addScript("classpath:org/springframework/batch/core/schema-h2.sql").setType(EmbeddedDatabaseType.H2).build();
     }
-
-    
 
     @Bean
     public TaskExecutor taskExecutor() {
@@ -32,6 +32,14 @@ public class StandaloneConfiguration {
         taskExecutor.setCorePoolSize(MIN_THREADS);
         taskExecutor.afterPropertiesSet();
         return taskExecutor;
+    }
+
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(final JobRegistry jobRegistry) throws Exception {
+        final JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor = new JobRegistryBeanPostProcessor();
+        jobRegistryBeanPostProcessor.setJobRegistry(jobRegistry);
+        jobRegistryBeanPostProcessor.afterPropertiesSet();
+        return jobRegistryBeanPostProcessor;
     }
 
 }
