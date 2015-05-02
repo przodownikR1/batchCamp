edb47494a7b43ae11d5b6f7e4d948cfb7424f88a
Slawomir Borowiec <przodownik@tlen.pl>
2015-05-02 23:02:55 +0200

    h2_web_server added

--- src/main/java/pl/java/scalatech/config/StandaloneConfiguration.java
+++ src/main/java/pl/java/scalatech/config/StandaloneConfiguration.java
@@ -5,13 +5,13 @@ import javax.sql.DataSource;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableBatchProcessing
+@Profile(value={"test","dev"})
 public class StandaloneConfiguration {
 
     @Bean
