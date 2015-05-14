75c84f651275947e71aabd458aafbc21fc00c591
Slawomir Borowiec <przodownik@tlen.pl>
2015-05-15 01:57:57 +0200

    launcher + sample.csv added

--- src/main/java/pl/java/scalatech/config/batch/BatchConfig.java
+++ src/main/java/pl/java/scalatech/config/batch/BatchConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.JobRegistry;
@@ -29,8 +30,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -39,7 +42,7 @@ import pl.java.scalatech.tasklet.HelloTasklet;
 
 @Configuration
 @EnableBatchProcessing
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @PropertySource("classpath:batch_mysql.properties")
 @Slf4j
 public class BatchConfig {
@@ -55,7 +58,7 @@ public class BatchConfig {
     private String dropScript;
     @Value("${batch.schema.script}")
     private String createScript;
-   
+
     @Autowired
     private JobBuilderFactory jobs;
 
@@ -64,26 +67,29 @@ public class BatchConfig {
 
     @Autowired
     private DataSource dataSource;
-    
-    @Bean 
+
+    @Bean
     public ResourceDatabasePopulator resourceDatabasePopulator() {
-        return new ResourceDatabasePopulator();
+        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+        resourceDatabasePopulator.addScript(new ClassPathResource(dropScript));
+        resourceDatabasePopulator.addScript(new ClassPathResource(createScript));
+        log.info("+++ resourceDbPopulator .... {},{}", dropScript, createScript);
+        return resourceDatabasePopulator;
     }
-    
+
+    @Bean
     public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
         DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
         dataSourceInitializer.setDataSource(dataSource);
-        resourceDatabasePopulator().addScript(new ClassPathResource(dropScript));
-        resourceDatabasePopulator().addScript(new ClassPathResource(createScript));
         dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator());
         return dataSourceInitializer;
     }
-    
+
     @PostConstruct
     public void init() {
-       log.info("+++  init BatchConfig ");
+        log.info("+++ init - > driverDb {}", driverDB);
     }
-    
+
     @Bean
     public Job job() {
         return jobs.get("HelloJob").start(step()).build();
@@ -103,21 +109,20 @@ public class BatchConfig {
     public PlatformTransactionManager transactionManager() {
         return new ResourcelessTransactionManager();
     }
-    
- 
-/*    @Bean
-   
+
+    @Bean
+    @Primary
     public DataSource dataSource() {
         System.err.println(driverDB);
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName(driverDB);
-       
+
         ds.setUrl(urlDB);
         ds.setUsername(userDB);
         ds.setPassword(passwdDB);
         return ds;
     }
-*/
+
     @Bean
     @DependsOn("jobExplorer")
     public JobOperator jobOperator(JobLauncher jobLauncher, JobRepository jobRepository, JobExplorer jobExplorer, JobRegistry jobRegistry) throws Exception {
@@ -139,7 +144,7 @@ public class BatchConfig {
     @Bean
     public JobRepository jobRepository(PlatformTransactionManager transactionManager, DataSource dataSource) throws Exception {
         JobRepositoryFactoryBean fb = new JobRepositoryFactoryBean();
-        log.info("+++  {}",dataSource.getConnection().getMetaData().getDriverName());
+        log.info("+++  {}", dataSource.getConnection().getMetaData().getDriverName());
         fb.setDataSource(dataSource);
         fb.setTransactionManager(transactionManager);
         fb.setTablePrefix("BATCH_");
---
+++ src/main/java/pl/java/scalatech/service/SimpleTaskService.java
@@ -0,0 +1,53 @@
+package pl.java.scalatech.service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.google.common.base.Stopwatch;
+
+@Slf4j
+//@Service
+public class SimpleTaskService {
+
+    private final JobLauncher jobLauncher;
+
+    private final Job job;
+
+    @Autowired
+    public SimpleTaskService(Job job, JobLauncher jobLauncher) {
+        this.job = job;
+        this.jobLauncher = jobLauncher;
+        log.info("+++  {}",SimpleTaskService.class.getSimpleName());
+    }
+
+    public void work() {
+        try {
+            log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> run a job begin.");
+
+            Stopwatch stopWatch = Stopwatch.createStarted();
+           
+            stopWatch.start();
+
+            Map<String, JobParameter> map = new HashMap<>();
+            map.put("date", new JobParameter(new Date()));
+
+            jobLauncher.run(job, new JobParameters(map));
+
+            stopWatch.stop();
+            log.info("execute time: " + stopWatch.elapsed(TimeUnit.MILLISECONDS) + " msec");
+
+            log.info("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< run a job end.");
+
+        } catch (Exception e) {
+            log.error("fatal: " + e.getMessage());
+        }
+    }
+}
